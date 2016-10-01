98d9a29231ee6350421e3f76c7c9c654760a3e9f
Stephane Thiell <sthiell@stanford.edu>
2016-09-30 17:15:00 -0700

    add cluset.py

---
+++ cluset.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from flask import Flask
+from ClusterShell.NodeSet import NodeSet
+
+app = Flask(__name__)
+
+@app.route("/fold/<nodeset>")
+def get_fold(nodeset):
+    return str(NodeSet(nodeset))
+
+@app.route("/expand/<nodeset>")
+def get_expand(nodeset):
+    return ' '.join(NodeSet(nodeset))
+
+@app.route("/count/<nodeset>")
+def get_count(nodeset):
+    return "%d" % len(NodeSet(nodeset))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
